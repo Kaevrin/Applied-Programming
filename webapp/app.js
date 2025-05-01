fb48a4d3bf0a48d66ee5054e48ddf518e9fbd822
Kaevrin <159499822+Kaevrin@users.noreply.github.com>
2025-05-01 15:39:35 -0600

    Adjusting code to work online.

--- webapp/app.js
+++ webapp/app.js
@@ -4,7 +4,12 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*", // or restrict to your GitHub Pages domain
+    methods: ["GET", "POST"]
+  }
+});
 
 app.use(express.static('public'));
 
